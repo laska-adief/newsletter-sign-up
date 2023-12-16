d9f17ba73d86606d9f705fe6d9cb41bb7a6e6d3a
laska-adief <laskaadief.la@gmail.com>
2023-12-16 11:02:13 +0700

    [Frontend Mentor - Newssletter Sign Up] newsletter destop styling

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,15 +1,17 @@
 import illustrationMobile from "./../assets/illustration-sign-up-mobile.svg";
+import illustrationDesktop from "./../assets/illustration-sign-up-desktop.svg";
 import iconList from "./../assets/icon-list.svg";
 
 const Newsletter = () => {
   const newsletterBenefit = ["Product discovery and building what matters", "Measuring to ensure updates are a success", "And much more!"];
   return (
-    <div>
-      <section className="flex flex-col sm:flex-row-reverse">
+    <div className="h-screen grid place-items-center">
+      <section className="flex flex-col sm:flex-row-reverse w-full sm:w-[80%] md:w-[80%] lg:w-[50%] bg-white rounded-lg sm:round sm:p-4">
         <div>
-          <img src={illustrationMobile} alt="illustration-sign-up" className="w-full" />
+          <img src={illustrationMobile} alt="illustration-sign-up" className="w-full block sm:hidden" />
+          <img src={illustrationDesktop} alt="illustration-sign-up" className="w-full h-full hidden sm:block" />
         </div>
-        <div className="p-4 pt-6 text-charcoal-grey font-medium">
+        <div className="p-4 pt-6 sm:p-6 lg:p-8 text-charcoal-grey font-medium">
           <h2 className="pb-4 font-bold text-3xl text-dark-slate-grey">Stay updated!</h2>
           <p className="pb-4">Join 60,000+ product managers receiving monthly updates on:</p>
           <div className="pb-8">
